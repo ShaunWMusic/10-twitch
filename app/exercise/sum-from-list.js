b576a92ca3b772a645857f35f3d0e536a96ea458
“Shaun <shaunwmusic@gmail.com>
2016-10-13 19:49:28 -0500

    Update javascript

--- app/exercise/sum-from-list.js
+++ app/exercise/sum-from-list.js
@@ -1,2 +1,6 @@
-export default function equal(message, message1) {
+export default function sumFromList(listOne) {
+  for (let i = 0; i < listOne.length; i++) {
+
+  }
+  return parseInt(listOne.innerText);
 }
