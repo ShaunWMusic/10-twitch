4df7980b59649b9b313ba6696ed0754efb666d5e
“Shaun <shaunwmusic@gmail.com>
2016-10-14 11:40:02 -0500

    Add li

--- app/exercise/sum-from-list.js
+++ app/exercise/sum-from-list.js
@@ -1,6 +1,8 @@
-export default function sumFromList(listOne) {
-  for (let i = 0; i < listOne.length; i++) {
-
+export default function sumFromList(list) {
+  console.log(list.children);
+  let count = 0;
+  for (let i = 0; i < list.children.length; i++) {
+    count = count + parseInt(list.children[i].innerText);
   }
-  return parseInt(listOne.innerText);
+  return count;
 }
