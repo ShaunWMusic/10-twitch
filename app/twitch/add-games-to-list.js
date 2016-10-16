70288b02963b8d5f01f3d99f794af1522ccf7427
“Shaun <shaunwmusic@gmail.com>
2016-10-15 21:38:27 -0500

    Test update

--- app/twitch/add-games-to-list.js
+++ app/twitch/add-games-to-list.js
@@ -1,3 +1,12 @@
-import createGameElemnt from './create-game-element';
+import createGameElement from './create-game-element';
 
-export default null;
+export default function showAllResults(parentEl, arr) {
+// loop through array of data
+  const createGameElement = [];
+  for (let i = 0; i < arr.length; i++) {
+    add = add + arr[i];
+  }
+  console.log(createGameElement);
+// create a game element from each item in the array of data
+// add the game element to the end of the parentEl
+}
