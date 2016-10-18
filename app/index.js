0dd3834620a6cf5c54481a97806347ad7495dd5b
“Shaun <shaunwmusic@gmail.com>
2016-10-18 13:05:14 -0500

    Start sidebar html

--- app/index.js
+++ app/index.js
@@ -1,7 +1,8 @@
 import 'whatwg-fetch';
-import 'data';
+import data from './data.json';
 
-import addGamesToList from 'twitch/add-games-to-list';
+import addGamesToList from './twitch/add-games-to-list';
 
 // Look up a `ul` element with a class called `results`
 // Add games to the results list based on the data from `data.json`
+const ul = document.querySelector('.results');
